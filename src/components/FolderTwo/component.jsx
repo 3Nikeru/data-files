26ca4ba87c27587d24278f95b3c6287752e42505
Alina Nikeru <alina.nikeru@gmail.com>
2023-03-04 00:38:46 +0200

    mobile layout

--- src/components/FolderTwo/component.jsx
+++ src/components/FolderTwo/component.jsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { Table, Card } from "antd";
 import ColumnGroup from "antd/es/table/ColumnGroup";
 import Column from "antd/es/table/Column";
 import useData from "../../hooks/data";
@@ -23,37 +23,39 @@ const FolderTwo = () =>{
           <h1>Folder Two</h1>
           <Link to='/' className="link"><ArrowLeftOutlined /></Link>
           {(folder_data !== undefined) ? 
-            <Table dataSource={folder_data} onChange={onChange} defaultSortOrder={sortOrder} 
-            defaultSortField={sortField}>
-              <ColumnGroup title="" >
-                      <Table.Column
-                         title=""
-                         dataIndex="icon"
-                         key="icon2"
-                         align="right"
-                         width="5%"
-                         render={ () => (
-                            <FileTwoTone />
-                         )}
-                      />
-                      <Column title="Name" dataIndex="name" key="name2" sorter = {(a, b) => a.name.length - b.name.length}
-                        sortDirections = {['descend']}/>
-              </ColumnGroup>
-              <Column title="Type" dataIndex="type" key="type2"/>
-              <Column title="Size" dataIndex="size" key="size2" defaultSortOrder='descend' sorter = {(a, b) => a.size - b.size} render={(size) => `${(size / 1024).toFixed(2)} KB`}/>
-              <Column title="Сreate Time" dataIndex="atime" key="atime2" render={(atime) => {
-              const date = new Date(atime * 1000); 
-              const formattedDate = date.toLocaleDateString();
-              return formattedDate;
-              }} 
-              sorter={(a, b) => a.atime - b.atime}/>
-              <Column title="Modification Time" dataIndex="mtime" key="mtime2" render={(mtime) => {
-              const date = new Date(mtime * 1000); 
-              const formattedDate = date.toLocaleDateString();
-              return formattedDate;
-              }}
-              sorter={(a, b) => a.mtime - b.mtime} />
-            </Table>
+            <Card>
+              <Table dataSource={folder_data} onChange={onChange} defaultSortOrder={sortOrder} 
+              defaultSortField={sortField} scroll={{ x: true }}>
+                <ColumnGroup title="" >
+                        <Table.Column
+                           title=""
+                           dataIndex="icon"
+                           key="icon2"
+                           align="right"
+                           width="5%"
+                           render={ () => (
+                              <FileTwoTone style={{ fontSize: '30px', color: '#08c' }}/>
+                           )}
+                        />
+                        <Column title="Name" dataIndex="name" key="name2" sorter = {(a, b) => a.name.length - b.name.length}
+                          sortDirections = {['descend']}/>
+                </ColumnGroup>
+                <Column title="Type" dataIndex="type" key="type2"/>
+                <Column title="Size" dataIndex="size" key="size2" defaultSortOrder='descend' sorter = {(a, b) => a.size - b.size} render={(size) => `${(size / 1024).toFixed(2)} KB`}/>
+                <Column title="Сreate Time" dataIndex="atime" key="atime2" render={(atime) => {
+                const date = new Date(atime * 1000); 
+                const formattedDate = date.toLocaleDateString();
+                return formattedDate;
+                }} 
+                sorter={(a, b) => a.atime - b.atime}/>
+                <Column title="Modification Time" dataIndex="mtime" key="mtime2" render={(mtime) => {
+                const date = new Date(mtime * 1000); 
+                const formattedDate = date.toLocaleDateString();
+                return formattedDate;
+                }}
+                sorter={(a, b) => a.mtime - b.mtime} />
+              </Table>
+            </Card>
             :
             <Table></Table>
             }
